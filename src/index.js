3c05ee75a5f74266e38ec0189216493288925099
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-07-08 14:23:53 -0400

    Added vision to user

--- src/index.js
+++ src/index.js
@@ -78,6 +78,18 @@ document.addEventListener('DOMContentLoaded', function(){
 
     function generateMap(){
 			let domMap = "<div>";
+			let x = [user.x, user.y]
+			let hash = {
+				top: [x[0],x[1]-1],
+				topRight: [x[0]+1,x[1]- 1],
+				right: [x[0]+ 1,x[1]],
+				bottomRight: [x[0]+1,x[1]+1],
+				bottom: [x[0],x[1]+1],
+				bottomLeft: [x[0]-1,x[1]+1],
+				left: [x[0]-1,x[1]],
+				topLeft: [x[0]-1,x[1]-1]
+			}
+			let values = Object.values(hash)
     	for (let y = 0; y < level.length; y++) {
         const row = level[y];
         domMap += "<div class='row'>";
@@ -88,9 +100,12 @@ document.addEventListener('DOMContentLoaded', function(){
 							domMap += `<div class="tile player"></div>`;
 						} else if (y === exit.y && x === exit.x) {
 							domMap += `<div class="tile exit"></div>`;
-						} else {
+						} else if (values.find(cord => cord[0] === x && cord[1] === y)) {
 							const tile = level[y][x];
             	domMap += `<div class="tile ${tile.type}"></div>`;
+						} else {
+							const tile = level[y][x];
+            	domMap += `<div class="tile black"></div>`;
 						}
         }
         domMap += "</div>";
