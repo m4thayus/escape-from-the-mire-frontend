4552e48aa28426b3c5f2f8cba03b50c52e257cca
Matt Williams <matt.seongjun@gmail.com>
2019-07-09 15:39:39 -0400

    Add chests and trees

--- src/cell.js
+++ src/cell.js
@@ -1,11 +1,11 @@
 class Cell {
     constructor(type){
-        this.status = 'hidden' 
+        this.status = 'show' 
         this.type = type
         if (type === 'wall') {
             this.texture = (Math.random() < 0.75) ? "water" : "tree"
         } else {
-            this.texture = ""
+            this.texture = null
         }
     }
 }
