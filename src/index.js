8c95779c2fc423a19379e5031fdbb27ab972b9a5
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-07-10 12:34:47 -0400

    Fixed movement bug

--- src/index.js
+++ src/index.js
@@ -29,3 +29,26 @@ let exit = {}
 document.addEventListener('keydown', () => {
 	user.movement(event)
 })
+
+// create user
+// fetch('http://127.0.0.1:3000/users', {
+// 		method: 'POST', 
+// 		headers: {
+//             'Content-Type': 'application/json',
+// 		}, 
+// 		body: JSON.stringify({name: "Nick", score: 100, character_class: "creator"})
+// 	})
+// 	.then(resp => resp.json())
+// 	.then(json => console.log(json))
+// 	.catch(err => console.log(err))
+// patch user
+// fetch('http://127.0.0.1:3000/users/3', {
+// 	method: 'PATCH', 
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 	}, 
+// 	body: JSON.stringify({id: 3, score: 150})
+// })
+// .then(resp => resp.json())
+// .then(json => console.log(json))
+// .catch(err => console.log(err))
--- src/user.js
+++ src/user.js
@@ -7,6 +7,7 @@ class User {
         this.kublaiX = null
         this.moveX = 0
         this.moveY = 0
+        this.score = 0
         this.vision = {
         }
     }
@@ -32,29 +33,36 @@ class User {
             case 'ArrowUp':
             case 'w':
                 this.moveY = -1
+                this.validateMovement()
                 break;
             case 'ArrowDown':
             case 's':
                 this.moveY = 1
+                this.validateMovement()
                 break;
             case 'ArrowLeft':
             case 'a':
                 this.moveX = -1
+                this.validateMovement()
                 break;
             case 'ArrowRight':
             case 'd':
                 this.moveX = 1
+                this.validateMovement()
                 break;
         }
+    }
 
-        Monster.takeTurn(); 
+    validateMovement(){
+        
         
         if ((this.y + this.moveY === exit.y && this.x + this.moveX === exit.x) || (this.y + this.moveY === entrance.y && this.x + this.moveX === entrance.x))  {
             this.kublaiY = this.y
             this.kublaiX = this.x
             this.x += this.moveX
             this.y += this.moveY
-     
+            this.score += 500
+            console.log(this.score)
             levelObj.generateMap()
     
             // Unused until front-end controls map size
@@ -64,19 +72,26 @@ class User {
             this.updateVision()	
             levelObj.generateMap()
         } else if (level[this.y + this.moveY][this.x + this.moveX].type != 'wall') {
-            
+            Monster.takeTurn(); 
             try {
                 if (level[this.y + this.moveY][this.x + this.moveX].texture === 'chest') {
                     console.log("CHEST COLLISION")
+                    this.score += 50
+                    console.log(this.score)
                     level[this.y + this.moveY][this.x + this.moveX].texture = null
                 }
                 if (level[this.y + this.moveY][this.x + this.moveX].texture === 'blood') {
                     console.log("BLOOD COLLISION")
+                    this.score += 50
+                    console.log(this.score)
                     level[this.y + this.moveY][this.x + this.moveX].texture = 'tentacle'
                 }
                 if (Monster.all.some(monster => monster.y === this.y + this.moveY && monster.x === this.x + this.moveX)) {
                     console.log("Monster COLLISION")
+                    this.score += 50
+                    console.log(this.score)
                 }
+                
                 this.kublaiY = this.y
                 this.kublaiX = this.x
                 this.x += this.moveX
